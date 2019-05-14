e197d264a2b7543071f51d346e139485a872ffc1
ChanMin <park.c@husky.neu.edu>
2019-05-14 02:29:21 -0400

    init and spring structure

---
+++ src/main/java/WebApplication/web_server_java/WebServerJavaApplication.java
@@ -0,0 +1,13 @@
+package WebApplication.web_server_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WebApplication/web_server_java/WebServerJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package WebApplication.web_server_java;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebServerJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
