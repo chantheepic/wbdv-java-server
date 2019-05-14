7867332b113d879a7ae7233917c32fee112d5158
ChanMin <park.c@husky.neu.edu>
2019-05-14 02:32:56 -0400

    created register functionality template, css, js;

---
+++ src/main/webapp/register/register.component.client.js
@@ -0,0 +1,22 @@
+
+(function () {
+  $('#alert').hide();
+
+  $('#registerBtn').click(function () {
+    if (PswCheck()) {
+      document.location.replace('../profile/profile.template.client.html');
+    }
+  });
+
+  function PswCheck() {
+    if ($('#usernameFld').val().length > 0 &&
+      $('#passwordFld').val().length > 0 &&
+      $('#passwordFld').val() === $('#verifyPasswordFld').val()) {
+      $('#alert').hide();
+      return true;
+    } else {
+      $('#alert').fadeIn();
+      return false;
+    }
+  }
+})();
